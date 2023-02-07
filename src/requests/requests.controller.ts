c9036c5e269c01d058ab19f50c71817b8cba8621
ilmossp <ilyamodeni@gmail.com>
2023-02-07 20:11:28 +0100

    added int parse pip to requests

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,17 +1,15 @@
-import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { UserService } from 'src/users/user.service';
+import { Controller, Get, HttpCode, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 
 @Controller('/user/:user/requests/')
 export class RequestsController {
   constructor(
-    private readonly requestService: RequestsService,
-    private readonly userService: UserService,
+    private readonly requestService: RequestsService
   ) {}
-
+  
   @HttpCode(200)
   @Get(':id')
-  async getRequestsByUserId(@Param('id') id: number) {
+  async getRequestsByUserId(@Param('id',ParseIntPipe) id: number) {
     const requests = await this.requestService.requests({
       where: { id, accepted: false },
     });
@@ -20,14 +18,14 @@ export class RequestsController {
 
   @HttpCode(200)
   @Post(':to')
-  async sendRequest(@Param('user') user: number, @Param('to') to: number) {
+  async sendRequest(@Param('user',ParseIntPipe) user: number, @Param('to',ParseIntPipe) to: number) {
     const request = await this.requestService.sendRequest(user, to);
     return request;
   }
 
   @HttpCode(200)
   @Post('accept/:id')
-  async accept(@Param('id') id: number) {
+  async accept(@Param('id',ParseIntPipe) id: number) {
     const request = await this.requestService.acceptRequest(id);
     return request;
   }
