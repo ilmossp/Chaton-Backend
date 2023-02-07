c10330830f73866935fa9725ba485c22745066f6
ilmossp <ilyamodeni@gmail.com>
2023-02-07 22:27:09 +0100

    enabled whitelist on global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
   app.useGlobalPipes(new ValidationPipe({
-    transform: true
+    transform: true,
+    whitelist: true
   }))
   app.use(
     session({
