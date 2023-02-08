735916148d49cde750d7336693c4ec79d502eacd
ilmossp <ilyamodeni@gmail.com>
2023-02-08 22:47:57 +0100

    fix: moved chat to own directory

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismModule } from 'src/prisma-service/prisma.module';
-import { MessageGateWay } from './Gateway/messages.gateway';
+
 import { MessageService } from './message.service';
 
 @Module({
     imports: [PrismModule],
-    providers:[MessageService,MessageGateWay]
+    providers:[MessageService],
+    exports: [MessageService]
 })
 export class MessageModule {}
